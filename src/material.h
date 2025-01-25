19c429145ed210f75be7c5847b26484224fcbd04
Tam <mthanimr24@gmail.com>
2025-01-25 19:40:37 +0000

    basic attempt at unlit material (epic fail)

--- src/material.h
+++ src/material.h
@@ -35,16 +35,35 @@ class lambertian : public material {
 
 class metal : public material {
   public:
-    metal(const color& albedo) : albedo(albedo) {}
+    metal(const color& albedo, double fuzz) : albedo(albedo), fuzz(fuzz < 1 ? fuzz : 1) {}
 
     bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered)
     const override {
         vec3 reflected = reflect(r_in.direction(), rec.normal);
+        reflected = unit_vector(reflected) + (fuzz * random_unit_vector());
         scattered = ray(rec.p, reflected);
         attenuation = albedo;
-        return true;
+        return (dot(scattered.direction(), rec.normal) > 0);
+
     }
 
   private:
     color albedo;
+    double fuzz;
+};
+
+class unlit : public material {
+  public:
+  unlit(const color& albedo) : albedo(albedo){}
+
+  bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const override {
+    auto scatter_direction = rec.normal;
+
+    scattered = ray(rec.p, -scatter_direction);
+    attenuation = albedo;
+    return false; 
+  }
+
+  private:
+  color albedo;
 };
