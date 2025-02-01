3148a9fc4391ffbfac4270ff5a17b5e6630d2695
Tam <mthanimr24@gmail.com>
2025-02-01 15:16:42 +0000

    camera position, lookat and fov

--- src/main.cpp
+++ src/main.cpp
@@ -17,24 +17,27 @@ int main() {
 
     auto material_ground = std::make_shared<lambertian>(color(0.8, 0.8, 0.0));
     auto material_center = std::make_shared<lambertian>(color(0.1, 0.2, 0.5));
-    auto material_left   = std::make_shared<dielectric>(1.00 / 1.33);
+    auto material_left   = std::make_shared<dielectric>(1.50);
+    auto material_bubble = std::make_shared<dielectric>(1.00 / 1.50);
     auto material_right  = std::make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
 
     world.add(std::make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
     world.add(std::make_shared<sphere>(point3( 0.0,    0.0, -1.2),   0.5, material_center));
     world.add(std::make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
+    world.add(std::make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.4, material_bubble));
     world.add(std::make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
-
-
-    //auto material_center = std::make_shared<unlit>(color(0.1, 0.5, 0.5));
-    //world.add(std::make_shared<sphere>(point3( 0.0,    0.0, -1.2),   0.5, material_center));
 
     camera cam;
 
-    cam.aspect_ratio = 16.0 / 9.0;
-    cam.width  = 400;
+    cam.aspect_ratio      = 16.0 / 9.0;
+    cam.width             = 400;
     cam.samples_per_pixel = 100;
     cam.max_depth         = 50;
+
+    cam.vfov     = 40;
+    cam.lookfrom = point3(-2,2,1);
+    cam.lookat   = point3(0,0,-1);
+    cam.vup      = vec3(0,1,0);
 
     cam.render(world);
 
