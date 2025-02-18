dc4860fbb0fcf60ec21c8f2ebe5127b9d8b68f16
Tam <mthanimr24@gmail.com>
2025-02-18 17:32:01 +0000

    Motion blur && bounding box heirarchy

--- src/hittable.h
+++ src/hittable.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "rtweekend.h"
+#include "aabb.h"
 
 class material;
 
@@ -27,4 +28,7 @@ class hittable {
     virtual ~hittable() = default;
 
     virtual bool hit(const ray& r, interval ray_t, hit_record& rec) const = 0;
+
+    virtual aabb bounding_box() const = 0;
+
 };
