8dc793c34a14c5d6f4f9c3cc7829b125ddd65c2a
Tam <mthanimr24@gmail.com>
2025-04-02 15:28:07 +0100

    Box

--- src/main.cpp
+++ src/main.cpp
@@ -23,6 +23,7 @@ enum scene {
         mix_mat_test,
         image_transparency_test,
         simple_light,
+        cornell_box,
     };
 
 void bouncingSpheres() {
@@ -287,9 +288,45 @@ void simpleLight() {
     cam.render(world);
 }
 
+void cornellBox() {
+    hittable_list world;
+
+    auto red   = std::make_shared<lambertian>(color(.65, .05, .05));
+    auto white = std::make_shared<lambertian>(color(.73, .73, .73));
+    auto green = std::make_shared<lambertian>(color(.12, .45, .15));
+    auto light = std::make_shared<diffuse_light>(color(15, 15, 15));
+
+    world.add(std::make_shared<quad>(point3(555,0,0), vec3(0,555,0), vec3(0,0,555), green));
+    world.add(std::make_shared<quad>(point3(0,0,0), vec3(0,555,0), vec3(0,0,555), red));
+    world.add(std::make_shared<quad>(point3(343, 554, 332), vec3(-130,0,0), vec3(0,0,-105), light));
+    world.add(std::make_shared<quad>(point3(0,0,0), vec3(555,0,0), vec3(0,0,555), white));
+    world.add(std::make_shared<quad>(point3(555,555,555), vec3(-555,0,0), vec3(0,0,-555), white));
+    world.add(std::make_shared<quad>(point3(0,0,555), vec3(555,0,0), vec3(0,555,0), white));
+
+    world.add(box(point3(130, 0, 65), point3(295, 165, 230), white));
+    world.add(box(point3(265, 0, 295), point3(430, 330, 460), white));
+
+    camera cam;
+
+    cam.aspect_ratio      = 1.0;
+    cam.width             = 600;
+    cam.samples_per_pixel = 200;
+    cam.max_depth         = 50;
+    cam.background        = color(0,0,0);
+
+    cam.vfov     = 40;
+    cam.lookfrom = point3(278, 278, -800);
+    cam.lookat   = point3(278, 278, 0);
+    cam.vup      = vec3(0,1,0);
+
+    cam.defocus_angle = 0;
+
+    cam.render(world);
+}
+
 int main() { 
 
-    switch (image_transparency_test) {
+    switch (cornell_box) {
         case bouncing_spheres        : bouncingSpheres();          break;
         case checkered_spheres       : checkeredSpheres();         break;
         case textured_sphere         : texturedSphere();           break;
@@ -298,6 +335,7 @@ int main() {
         case mix_mat_test            : mixMaterial();              break;
         case image_transparency_test : imageTransparencyMapping(); break;
         case simple_light            : simpleLight();              break;
+        case cornell_box             : cornellBox();               break;
     }
     
     return 0;
